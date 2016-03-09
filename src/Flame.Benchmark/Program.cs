d8c85d550a241e81cad38436304cfeefdd6df9bf
jonathanvdc <jonathan.vdc@outlook.com>
2016-03-09 18:01:12 +0100

    Added Flame libs

---
+++ src/Flame.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Flame.Front.Cli;
+
+namespace Flame.Benchmark
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var compiler = new ConsoleCompiler("flame-benchmark", "the Flame benchmark writer", "https://github.com/jonathanvdc/flame-benchmark/releases");
+            Environment.Exit(compiler.Compile(args));
+        }
+    }
+}
